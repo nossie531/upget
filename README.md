e903110958769b89e9646b8807362583947dc0ce
okd <nossie531@gmail.com>
2023-04-04 04:50:44 +0900

    New.

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# upget
+upget
+===
+
 Super simple trait that patterns the value "updae" and "get".
+
+*The author of this crate is not good at English.*  
+*Forgive me if the document is hard to read.*
+
+The `Upget` trait only has an `upget` method, which just updates `self` value
+with a closure and then retrieves it. That's all there is to it, but it allows
+you to refactor common code patterns.
+
+## Target Code
+
+For example, the following code can be refactored...
+
+```rust
+fn clone_with_sort(vec: &Vec<String>) -> Vec<String> {
+    let mut result = vec.clone();
+    result.sort();
+    result
+}
+```
+
+as follows.
+
+```rust
+fn clone_with_sort(vec: &Vec<String>) -> Vec<String> {
+    vec.clone().upget(|x| x.sort())
+}
+```
+
+Such refactoring has the following benefits.
+* Simplification of code.
+* Elimination of intermediate variables.
+* Elimination of the `mut` specification.
---
+++ src/lib.rs
@@ -0,0 +1,48 @@
+/*! Provider of [`Upget`].
+
+*The author of this crate is not good at English.*  
+*Forgive me if the document is hard to read.*
+
+The [`Upget`] trate can be used for refactoring as follows.
+
+Before refactoring.
+```
+fn clone_with_sort(vec: &Vec<String>) -> Vec<String> {
+    let mut result = vec.clone();
+    result.sort();
+    result
+}
+```
+
+After refactoring.
+```
+# use crate::upget::Upget;
+fn clone_with_sort(vec: &Vec<String>) -> Vec<String> {
+    vec.clone().upget(|x| x.sort())
+}
+```
+*/
+
+#![no_std]
+
+/// Trait that handles common patterns of updating and getting value.
+pub trait Upget {
+    /// Update self in the specified way and get the result.
+    /// 
+    /// # Examples
+    /// 
+    /// ```
+    /// # use crate::upget::Upget;
+    /// assert_eq!([3, 2, 1].upget(|x| x.sort()), [1, 2, 3]);
+    /// ```
+    fn upget<F>(mut self, f: F) -> Self
+    where
+        Self: Sized,
+        F: FnOnce(&mut Self),
+    {
+        f(&mut self);
+        self
+    }
+}
+
+impl<T> Upget for T {}
